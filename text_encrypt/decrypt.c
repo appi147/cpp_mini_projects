d38ee2a7b3149bcd2285a3dbe00379c084707b98
appi <arpitkumar147@gmail.com>
2017-03-02 01:04:09 +0000

    made code readable

--- text_encrypt/decrypt.c
+++ text_encrypt/decrypt.c
@@ -9,34 +9,34 @@ int main()
 {
 	FILE *ncrypt , *KEY , *dcrypt;
 	char name[FILENAME] , temp;
-	ncrypt = fopen ( "encrypted.txt" , "r" );
-	if ( ncrypt == NULL )
+	ncrypt = fopen("encrypted.txt","r");
+	if (ncrypt==NULL)
 	{
-		puts ( "Error loading encrypted file" );
-		exit ( 1 );
+		puts("Error loading encrypted file");
+		exit(1);
 	}
-	dcrypt = fopen ( "decrypted.txt" , "w" );
+	dcrypt = fopen("decrypted.txt","w");
 	char key[KEYNAME];
-	printf ( "Enter key to decrypt\n" );
-	scanf ( "%s" , key);
+	printf ("Enter key to decrypt\n");
+	scanf ("%s",key);
 	int length,i;
-	length = strlen ( key );
+	length = strlen(key);
 	char keycopy[length];
-	for ( i = 0 ; i < length ; i++ )
+	for (i = 0; i < length; i++)
 	{
-		key[i] = toupper ( key[i] );
+		key[i] = toupper(key[i]);
 		key[i] -= 65;
 	}
 	int c = 0;
-	while ( true )
+	while (true)
 	{
-		c % = length;
-		temp = fgetc ( ncrypt );
-		if ( temp == EOF )
+		c %= length;
+		temp = fgetc(ncrypt);
+		if (temp == EOF)
 		break;
 		else
 		{
-			if ( temp <= 90 && temp >= 65 )
+			if (temp <= 90 && temp >= 65)
 			{
 				temp -= 65;
 				temp += 26;
@@ -44,9 +44,9 @@ int main()
 				c++;
 				temp %= 26;
 				temp += 65;
-				fputc ( temp , dcrypt );
+				fputc(temp , dcrypt);
 			}
-			else if ( temp >= 97 && temp <= 122 )
+			else if (temp >= 97 && temp <= 122)
 			{
 				temp -= 97;
 				temp += 26;
@@ -54,17 +54,17 @@ int main()
 				c++;
 				temp %= 26;
 				temp += 97;
-				fputc ( temp , dcrypt );
+				fputc(temp , dcrypt);
 			}
 			else
 			{
-				fputc ( temp , dcrypt );
+				fputc(temp , dcrypt);
 			}
 		}
 	}
-	printf ( "File successfully decrypted\n" );
-	fclose ( ncrypt );
-	fclose ( dcrypt );
+	printf ("File successfully decrypted\n");
+	fclose (ncrypt);
+	fclose (dcrypt);
 	return 0;
 }
 
--- text_encrypt/encrypt.c
+++ text_encrypt/encrypt.c
@@ -8,79 +8,79 @@ int main()
 {
 	FILE *fileread , *filewrite , *KEY;
 	char name[FILENAME] , temp;
-	printf ( "Enter name of file\n" );
-	scanf ( "%s" , name );
-	fileread = fopen ( name , "r" );
-	if ( fileread == NULL )
+	printf ("Enter name of file\n");
+	scanf ("%s", name);
+	fileread = fopen (name,"r");
+	if (fileread == NULL)
 	{
-		puts ( "Error loading file" );
-		exit ( 1 );
+		puts ("Error loading file");
+		exit (1);
 	}
-	filewrite = fopen ( "encrypted.txt" , "w" );
-	if ( filewrite == NULL )
+	filewrite = fopen ("encrypted.txt","w");
+	if (filewrite == NULL)
 	{
-		puts ( "Error writing file" );
-		exit ( 2 );
+		puts ("Error writing file");
+		exit (2);
 	}
 	char key[KEYNAME];
-	printf ( "Enter key to encrypt\n" );
-	scanf ( "%s" , key );
-	int length , i;
-	length = strlen ( key );
+	printf ("Enter key to encrypt\n");
+	scanf ("%s", key);
+	int length, i;
+	length = strlen (key);
 	char keycopy[length];
-	for ( i = 0 ; i < length ; i++ )
+	for (i = 0; i < length; i++)
 	{
 		keycopy[i] = key[i];
-		key[i] = toupper ( key[i] );
+		key[i] = toupper (key[i]);
 		key[i] -= 65;
 	}
 	keycopy[length] = key[length];
 	int c = 0;
-	while ( true )
+	while (true)
 	{
 		c %= length;
-		temp = fgetc ( fileread );
-		if ( temp == EOF )
+		temp = fgetc (fileread);
+		if (temp == EOF)
 		break;
 		else
 		{
-			if ( temp <= 90 && temp >= 65 )
+			if (temp <= 90 && temp >= 65)
 			{
 				temp -= 65;
 				temp += key[c];
 				c++;
 				temp %= 26;
 				temp += 65;
-				fputc ( temp , filewrite );
+				fputc (temp, filewrite);
 			}
-			else if ( temp >= 97 && temp <= 122 )
+			else if (temp >= 97 && temp <= 122)
 			{
 				temp -= 97;
 				temp += key[c];
 				c++;
 				temp %= 26;
 				temp += 97;
-				fputc ( temp , filewrite );
+				fputc (temp, filewrite);
 			}
 			else
 			{
-				fputc ( temp , filewrite );
+				fputc (temp, filewrite);
 			}
 		}
 	}
-	printf ( "File successfully encrypted\n" );
-	fclose ( fileread );
-	fclose ( filewrite );
-	KEY = fopen ( "key.txt" , "w" );
-	if ( KEY == NULL )
+	printf ("File successfully encrypted\n");
+	fclose (fileread);
+	fclose (filewrite);
+	KEY = fopen ("key.txt","w");
+	if (KEY == NULL)
 	{
-		puts ( "Key could not be printed" );
+		puts ("Key could not be printed");
 	}
-	for ( i = 0; i < length ; i++ )
+	for (i = 0; i < length; i++)
 	{
 		temp = keycopy[i];
-		fputc ( temp , KEY );
+		fputc (temp , KEY);
 	}
-	fclose ( KEY );
+	fclose (KEY);
 	return 0;
 }
